76f7253ad171d1d895923b6e8a9a9cbb0e2afbf1
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2021-12-19 11:32:33 -0500

    Handling database fetch failure

--- src/main/java/com/DSD/HAWK/PROJECT/aggregator/Service/Implementation/BlogAggregatorImpl.java
+++ src/main/java/com/DSD/HAWK/PROJECT/aggregator/Service/Implementation/BlogAggregatorImpl.java
@@ -3,6 +3,7 @@ package com.DSD.HAWK.PROJECT.aggregator.Service.Implementation;
 import com.DSD.HAWK.PROJECT.aggregator.Request.PostBlogRequest;
 import com.DSD.HAWK.PROJECT.aggregator.Response.Blog.BlogResponse;
 import com.DSD.HAWK.PROJECT.aggregator.Service.BlogAggregator;
+import com.DSD.HAWK.PROJECT.lib.Constant;
 import com.DSD.HAWK.PROJECT.model.Blog;
 import com.DSD.HAWK.PROJECT.repository.Service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,15 @@ public class BlogAggregatorImpl implements BlogAggregator {
     @Override
     public List<BlogResponse> getAllBlogs(String email) {
         List<BlogResponse> ls = new ArrayList<>();
-        List<Blog> blogs = blogService.getAllBlogs(email);
-        blogs.stream().forEach(blog -> ls.add(new BlogResponse(blog.getId(),blog.getEmail(),blog.getBlogTitle(), blog.getBlogBody())));
-        return ls;
+        try {
+            List<Blog> blogs = blogService.getAllBlogs(email);
+            blogs.stream().forEach(blog -> ls.add(new BlogResponse(blog.getId(),blog.getEmail(),blog.getBlogTitle(), blog.getBlogBody())));
+            return ls;
+        }catch (Exception ex){
+            System.out.println("Unable to fetch info from database");
+            return Constant.blogList;
+        }
+
     }
 
     @Override
